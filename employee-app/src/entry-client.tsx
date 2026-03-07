459c2b925908be11e82595507b95ee9fd1095e3b
rgalaxy <hollandmakermh@gmail.com>
2026-03-07 20:17:23 +0700

    config: update vite config to not use SSR with express

--- employee-app/src/entry-client.tsx
+++ employee-app/src/entry-client.tsx
@@ -1,11 +1,10 @@
 import "./index.css";
 import { StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-hydrateRoot(
-  document.getElementById("root") as HTMLElement,
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
