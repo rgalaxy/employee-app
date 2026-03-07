736ca4e8aa15e76acc134c302857875325561dd4
rgalaxy <hollandmakermh@gmail.com>
2026-03-07 17:42:43 +0700

    feat: add jest and jest config

---
+++ employee-app/jest.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterFramework: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+          moduleResolution: 'node',
+          isolatedModules: true,
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+          noUnusedLocals: false,
+          noUnusedParameters: false,
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(png|jpg|jpeg|gif|svg|webp)$': '<rootDir>/src/__mocks__/fileMock.cjs',
+  },
+  testMatch: ['**/__tests__/**/*.test.{ts,tsx}', '**/*.test.{ts,tsx}'],
+};
